12462aff135711da315ebc136f85f392fcca6324
testuser <testproject692@gmail.com>
2024-08-28 13:36:28 +0000

    add the port in the main file

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ migrate = Migrate(app, db)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() 
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=80)
